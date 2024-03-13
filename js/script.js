3f17fc78393182b20ee437e6ffc2720252c12bb7
giovanna <giovanna.ssant@gmail.com>
2024-03-13 01:53:19 -0300

    search funcional

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-const apiKey = 'ff05728585944d398909cc8c684f6ed2'
-const baseURL = 'https://api.rawg.io/api'
-
 document.addEventListener('DOMContentLoaded', async() => {
-    let exibirJogos = document.querySelector('#jogos-lista')
 
     //fazendo a requisição de API após carregar a página
     try{
@@ -35,10 +31,67 @@ document.addEventListener('DOMContentLoaded', async() => {
 
         jogosLista.innerHTML = exibirJogos.join('')
 
-        console.log(jogos)
+        // console.log(jogos)
 
     } catch(error) {
-        console.error(error);
+        console.error(error)
         //captura de erros e exceções da API exibindo-as no console
     }
 })
+
+
+const pesquisar = document.querySelector('#searchtest')
+const buttonsearch = document.querySelector('#pesquisa-btn')
+const formPesquisa = document.querySelector('#pesquisa')
+
+formPesquisa.addEventListener('submit', function(event) {
+    event.preventDefault()
+    searchGames()
+})
+
+buttonsearch.addEventListener('click', function(event) {
+    event.preventDefault()
+    searchGames()
+})
+
+async function searchGames() {
+    const searchTerm = pesquisar.value // Corrigido para obter o valor do campo de pesquisa
+
+    jogosLista.style.display = "none"
+    //fazendo a requisição de API de forma assíncrona
+    try {
+        const resposta = await fetch(`https://api.rawg.io/api/games?key=ff05728585944d398909cc8c684f6ed2&search=${searchTerm}`)
+
+        if (!resposta.ok) {
+            throw new Error(`Erro de requisição: ${resposta.status} - ${resposta.statusText}`)
+            //verificando respostas do servidor
+        }
+        
+        const dataSearch = await resposta.json()
+        console.log(dataSearch)
+
+        const jogosNovos = dataSearch.results.map(jogo => ({
+            imagem: jogo.background_image,
+            nome: jogo.name,
+            lancamento: jogo.released ? jogo.released.substring(0, 4) : "Indisponível", // Verifica se released é nulo
+            genero: jogo.genres.map(genre => genre.name).join(', ')
+        }))
+
+        const exibirJogosNovos = jogosNovos.map(jogo => {
+            return `
+            <div class="jogo-unico">
+                <img class="imagemJogo" src='${jogo.imagem}'>
+                <p class="font-title-2">${jogo.nome}</p>
+                <p class="font-text-1 cor-6">(${jogo.lancamento})</p>
+                <p class="font-text-1 cor-3">${jogo.genero}</p>
+            </div>
+            `
+        })
+
+        buscaLista.innerHTML = exibirJogosNovos.join('')
+
+    } catch(error) {
+        console.error(error)
+        //captura de erros e exceções da API exibindo-as no console
+    }  
+}
